b8dc73c64eb69bc27b866cd1c53ae9729bf61c12
Colin de Koning <colinuithem@gmail.com>
2023-01-13 13:34:04 +0100

    home_registration geprobeert te fixen

--- registration/home_registration.php
+++ registration/home_registration.php
@@ -1,31 +1,13 @@
-<?php 
-session_start();
+<!DOCTYPE html>
+<html>
+<head>
+<title>HOME</title>
+<link rel="stylesheet" type="text/css" href="styles_login.css">
+</head>
+<body>
+<h1>Hello, colin</h1>
 
-if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
+<a href="logout.php">Logout</a>
 
-    ?>
-
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>HOME</title>
-    <link rel="stylesheet" type="text/css" href="styles_login.css">
-    </head>
-    <body>
-
-     <h1>Hello, colin</h1>
-
-     <a href="logout.php">Logout</a>
-
-    </body>
-    </html>
-
-    <?php 
-
-}
-else{
-     header("Location: index.php");
-     exit();
-}
-
- ?>
+</body>
+</html>
