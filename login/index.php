65cd93591428ef20091913aad02e28aafdd849de
KieferPln <kiefer.plender@gmail.com>
2023-01-13 14:39:39 +0100

    login page new

--- login/index.php
+++ login/index.php
@@ -1,20 +1,36 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" >
 <head>
-    <title>Login page</title>
-    <link rel="stylesheet" type="text/css" href="styles_login.css">
+  <meta charset="UTF-8">
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Rubik:400,700'><link rel="stylesheet" href="./styles_login.css">
+
 </head>
 <body>
-     <form action="login.php" method="post">
-        <h2>LOGIN</h2>
-        <?php if (isset($_GET['error'])) { ?>
+<form action="login.php" method="post">
+<?php if (isset($_GET['error'])) { ?>
             <p class="error"><?php echo $_GET['error']; ?></p>
         <?php } ?>
-        <label>Username</label>
-        <input type="text" name="uname" placeholder="Username"><br>
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Password"><br> 
-        <button type="submit">Login</button>
-     </form>
+<div class="login-form">
+  
+    <h1>Fill in your username and password:</h1>
+    
+    <div class="content">
+      <div class="input-field">
+        <input type="text" name="uname" placeholder="Username" autocomplete="nope">
+      </div>
+      <div class="input-field">
+        <input type="password" name="password" placeholder="Password" autocomplete="new-password">
+      </div>
+      <a href="#" class="link">Forgot Your Password?</a>
+    </div>
+    <div class="action">
+      <button type="button" onclick="href=/registration/index_.php">Register</button>
+      <button type="submit">Sign in</button>
+    </div>
+  </form>
+</div>
+
+<!-- <script  src="./script.js"></script> -->
+
 </body>
 </html>
