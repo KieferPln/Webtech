46541e9bdadae52aa1bb86b046702f976fb1c78b
KieferPln <kiefer.plender@gmail.com>
2023-01-16 16:15:56 +0100

    database link fix

--- login/login_query.php
+++ login/login_query.php
@@ -6,7 +6,7 @@
 	if(ISSET($_POST['login'])){
 		if($_POST['username'] != "" || $_POST['password'] != ""){
 			$username = $_POST['username'];
-			$password = hash('sha256', $_POST['password']);
+			$password = ∏hash('sha256', $_POST['password']);
 			$sql = "SELECT * FROM `users` WHERE `username`=? AND `password`=? ";
 			$query = $conn->prepare($sql);
 			$query->execute(array($username,$password));
