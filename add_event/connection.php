cccef4df2e77756bf6bd4cd219244f93f49bb787
deepeekayy <daankeller@gmail.com>
2023-01-17 14:28:11 +0100

    ...

--- add_event/connection.php
+++ add_event/connection.php
@@ -5,7 +5,7 @@ $password = "bWbkWfCXuNAqbWAtBvPTINkMHQqIjzwG";
 
 try {
 
-    $conn = new PDO( 'mysql:host=localhost;dbname=test_db', $username, $password);
+    $conn = new PDO( 'mysql:host=localhost;dbname=life_below_water', $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e) {
