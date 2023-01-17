14840158f4a6630ab6af99489b3027b8cbba5838
cdk100 <colinuithem@gmail.com>
2023-01-17 13:47:03 +0100

    de database connection aangepast

--- login/opendb.php
+++ login/opendb.php
@@ -1,7 +1,7 @@
 <?php
 	$db_username = 'kieferp';
 	$db_password = 'UkmtdEVlrYGOKYkGqeljPundaLYKpZin';
-	$conn = new PDO( 'mysql:host=localhost;dbname=test_db', $db_username, $db_password );
+	$conn = new PDO( 'mysql:host=localhost;dbname=life_below_water', $db_username, $db_password );
 	if(!$conn){
 		die("Fatal Error: Connection Failed!");
 	}
