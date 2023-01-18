4bc851e308bd5cf62fb1a4d96443dbb6c90450b1
Julius de Groot <jjdegroot2001@gmail.com>
2023-01-18 18:07:05 +0100

    💅

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const getRectTopById = (id) => {
 }
 
 const scrollTo = (top) => {
-    let main = document.getElementById('main')
     main.scrollTop = top
 }
 
 let nav_events = document.getElementById('nav_events')
 nav_events.addEventListener('click', () => scrollTo(getRectTopById('events')), true);
 
-let nav_targets = document.getElementById('nav_targets')
-nav_targets.addEventListener('click', () => scrollTo(getRectTopById('targets')), true);
+// let nav_targets = document.getElementById('nav_targets')
+// nav_targets.addEventListener('click', () => scrollTo(getRectTopById('targets')), true);
 
 let popup = document.getElementById('popup')
+let infoPopup = document.getElementById('info-popup')
 let blurdiv = document.getElementById('blur')
 var popupIsTriggered = false
+var infoPopupIsTriggerd = false
 
 const togglePopup = () => {
     if (!popupIsTriggered) {
@@ -27,7 +28,25 @@ const togglePopup = () => {
         popup.style.transform = "translateY(100%)"
         main.style.overflow = 'auto'
     }
-
     popupIsTriggered = !popupIsTriggered
 }
 
+const setInfo = (type) => {
+
+}
+
+const toggleInfoPopup = (type) => {
+    if (!infoPopupIsTriggerd) {
+        infoPopup.style.opacity = "1"
+        infoPopup.style.visibility = "visible"
+        main.style.overflow = 'hidden'
+    }
+    else {
+        infoPopup.style.opacity = "0"
+        infoPopup.style.visibility = "hidden"
+        main.style.overflow = 'auto'
+    }
+    infoPopupIsTriggerd = !infoPopupIsTriggerd
+
+    console.log(type)
+}
