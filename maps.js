df270a2ac7334bc72d2c5fd8b736dc2493a5e832
Julius de Groot <jjdegroot2001@gmail.com>
2023-01-18 13:40:55 +0100

    ....

--- maps.js
+++ maps.js
@@ -4,14 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 // Initialize and add the map
+
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
+    const uluru = { lat: 51.295418, lng: 30.222702 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 10,
         center: uluru,
-        disableDefaultUI: true
+        disableDefaultUI: true,
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
