460c379b8e90d4738c7b4ae2623a6ca33592e613
MilanTool <milan.tool@student.uva.nl>
2023-01-19 16:28:33 +0100

    nu echt

--- login/index.php
+++ login/index.php
@@ -7,8 +7,8 @@
 	<link rel="stylesheet" type="text/css" href="./styles_reg.css"/>
 	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/var/www/html/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/var/www/html/favicon/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 </head>
