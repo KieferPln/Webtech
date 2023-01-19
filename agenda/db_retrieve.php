028edce029a2d8f5eb5c8a27c802350953adab96
”deepeekayy” <daankeller@gmail.com>
2023-01-19 08:15:35 +0100

    ..

---
+++ agenda/db_retrieve.php
@@ -0,0 +1,5 @@
+
+// om van een specifiek event de informatie aan variabelen toe te wijzen is dit de sql query
+
+SELECT @event_name := name, @event_date := date, @event_url := url, @event_location := location, 
+@desc := description FROM events WHERE eventid = '@eventid';
