89c788f2618dfe9c1418a61c4f98c43e5242156c
deepeekayy <daankeller@gmail.com>
2023-01-22 14:25:55 +0100

    toegang tot connection gewijzigd.

--- connection.php
+++ connection.php
@@ -1,16 +1,15 @@
-<?php
+// this file is one folder higher on the server than the other files to prevent people from
+// accessing it.
 
+<?php
 $username = "daank";
 $password = "bWbkWfCXuNAqbWAtBvPTINkMHQqIjzwG";
 
 try {
-
     $conn = new PDO( 'mysql:host=localhost;dbname=life_below_water', $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e) {
-
     echo "Connection Failed" .$e->getMessage();
 }
-
 ?>
