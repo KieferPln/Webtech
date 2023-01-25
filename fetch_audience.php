865fcf48a90579e2f30ba8f9aab5136bd5194073
Colin de Koning <colinuithem@gmail.com>
2023-01-25 14:02:39 +0100

    fetch bestanden aangemaakt

---
+++ fetch_audience.php
@@ -0,0 +1,8 @@
+<?php
+require('./connection.php');
+session_start();
+$sql = $conn->prepare('SELECT * FROM events_audience');
+$sql->execute();
+$fetch = $sql->fetchAll();
+echo json_encode($fetch)
+?>
