6879d861ead50c24f9b8e0a28fe2e5c33e0e0cb5
KieferPln <kiefer.plender@gmail.com>
2023-01-25 15:11:03 +0100

    visits en bubble cookies!

--- bubbles.js
+++ bubbles.js
@@ -1,61 +1,136 @@
+// const bubble_layer = document.getElementById('bubble-layer')
+// let main = document.getElementById('main')
+// var bubbleCount = 0
+
+// const delay = (time) => {
+//     return new Promise(resolve => setTimeout(resolve, time));
+// }
+
+// const getRandomArbitrary = (min, max) => {
+//     return Math.random() * (max - min) + min;
+// }
+
+// const generateSpeed = (amount) => {
+//     return [...Array(amount)].map(_ => getRandomArbitrary(15, 40))
+// }
+
+// const moveUp = async (element, speed) => {
+//     var size = `scale(${getRandomArbitrary(0.35, 1)}`
+//     var offset = -50
+//     const shouldSwerve = Math.random() < 0.5
+//     const height = getRandomArbitrary(15, 60)
+
+//     for (let i = 0; i < height; i++) {
+//         if (shouldSwerve) { offset -= 1 }
+//         else { offset += 0 }
+
+//         element.style.transform = `translate(${offset + i * 2}%,${-i * 2}px) ${size})`
+//         element.style.opacity = 1 - (i / height).toFixed(3)
+
+//         await delay(speed)
+//     }
+//     element.style.opacity = 0
+// }
+
+// const createElement = async (speed, position) => {
+//     const bubble = document.createElement("span");
+//     bubble.classList.add('bubble')
+//     bubble.style.left = position.x + 'px';
+//     bubble.style.top = position.y + 'px';
+//     header.appendChild(bubble);
+//     await moveUp(bubble, speed)
+//     header.removeChild(bubble)
+// }
+
+// const createBubbles = (e) => {
+//     bubbleCount++
+//     addCountToCookie(bubbleCount)
+//     const count = Math.round(getRandomArbitrary(3, 8))
+//     const speeds = generateSpeed(count)
+//     const position = { x: e.clientX, y: e.clientY + main.scrollTop }
+
+//     for (let i = 0; i < count; i++) {
+//         createElement(speeds[i], position)
+//     }
+// }
+
 const bubble_layer = document.getElementById('bubble-layer')
-let main = document.getElementById('main')
-var bubbleCount = 0
+        let main = document.getElementById('main')
+        var bubbleCount = getCountFromCookie()
 
-const delay = (time) => {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
+        // function addCountToCookie(count) {
+        //     var d = new Date();
+        //     d.setTime(d.getTime() + (365*24*60*60*1000));
+        //     var expires = "expires="+ d.toUTCString();
+        //     document.cookie = "bubbleCount=" + count + ";" + expires + ";path=/";
+        // }
 
-const getRandomArbitrary = (min, max) => {
-    return Math.random() * (max - min) + min;
-}
+        // function getCountFromCookie() {
+        //     var name = "bubbleCount=";
+        //     var decodedCookie = decodeURIComponent(document.cookie);
+        //     var ca = decodedCookie.split(';');
+        //     for(var i = 0; i <ca.length; i++) {
+        //         var c = ca[i];
+        //         while (c.charAt(0) == ' ') {
+        //             c = c.substring(1);
+        //         }
+        //         if (c.indexOf(name) == 0) {
+        //             return c.substring(name.length, c.length);
+        //         }
+        //     }
+        //     return "";
+        // }
 
-const generateSpeed = (amount) => {
-    return [...Array(amount)].map(_ => getRandomArbitrary(15, 40))
-}
+        const delay = (time) => {
+            return new Promise(resolve => setTimeout(resolve, time));
+        }
 
-const moveUp = async (element, speed) => {
-    var size = `scale(${getRandomArbitrary(0.35, 1)}`
-    var offset = -50
-    const shouldSwerve = Math.random() < 0.5
-    const height = getRandomArbitrary(15, 60)
+        const getRandomArbitrary = (min, max) => {
+            return Math.random() * (max - min) + min;
+        }
 
-    for (let i = 0; i < height; i++) {
-        if (shouldSwerve) { offset -= 1 }
-        else { offset += 0 }
+        const generateSpeed = (amount) => {
+            return [...Array(amount)].map(_ => getRandomArbitrary(15, 40))
+        }
 
-        element.style.transform = `translate(${offset + i * 2}%,${-i * 2}px) ${size})`
-        element.style.opacity = 1 - (i / height).toFixed(3)
+        const moveUp = async (element, speed) => {
+            var size = `scale(${getRandomArbitrary(0.35, 1)}`
+            var offset = -50
+            const shouldSwerve = Math.random() < 0.5
+            const height = getRandomArbitrary(15, 60)
 
-        await delay(speed)
-    }
-    element.style.opacity = 0
-}
+            for (let i = 0; i < height; i++) {
+                if (shouldSwerve) { offset -= 1 }
+                else { offset += 0 }
 
-const createElement = async (speed, position) => {
-    const bubble = document.createElement("span");
-    bubble.classList.add('bubble')
-    bubble.style.left = position.x + 'px';
-    bubble.style.top = position.y + 'px';
-    header.appendChild(bubble);
-    await moveUp(bubble, speed)
-    header.removeChild(bubble)
-}
+                element.style.transform = `translate(${offset + i * 2}%,${-i * 2}px) ${size})`
+                element.style.opacity = 1 - (i / height).toFixed(3)
 
-const createBubbles = (e) => {
-    bubbleCount++
-    addCountToCookie(bubbleCount)
-    const count = Math.round(getRandomArbitrary(3, 8))
-    const speeds = generateSpeed(count)
-    const position = { x: e.clientX, y: e.clientY + main.scrollTop }
+                await delay(speed)
+            }
+            element.style.opacity = 0
+        }
 
-    for (let i = 0; i < count; i++) {
-        createElement(speeds[i], position)
-    }
-}
+        const createElement = async (speed, position) => {
+            const bubble = document.createElement("span");
+            bubble.classList.add('bubble')
+            bubble.style.left = position.x + 'px';
+            bubble.style.top = position.y + 'px';
+            header.appendChild(bubble);
+            await moveUp(bubble, speed)
+            header.removeChild(bubble)
+        }
 
-const addCountToCookie = (bubbleCount) => {
-    
-}
-
-bubble_layer.addEventListener('mousedown', createBubbles, true);
+        const createBubbles = (e) => {
+            bubbleCount++
+            addCountToCookie(bubbleCount)
+            const count = Math.round(getRandomArbitrary(3, 8))
+            const speeds = generateSpeed(count)
+            const position = { x: e.clientX, y: e.clientY + main.scrollTop }
+            
+            for (let i = 0; i < count; i++) {
+                createElement(speeds[i], position)
+                }
+            }
+            
+    bubble_layer.addEventListener('mousedown', createBubbles, true);
--- cookies.js
+++ cookies.js
@@ -1,72 +1,65 @@
-// setCookie = (cName, cValue, expDays) => {
-//     let date = new Date();
-//     date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
-//     const expires = "expires=" + date.toUTCString();
-//     document.cookie = cName + "=" + cValue + "; " + expires + "; path =/";
-// }
-
-// getCookie = (cName) => {
-//     const name = cName + "=";
-//     const cDecoded = decodeURIComponent(document.cookie); 
-//     const cArr = cDecoded.split("; ");
-//     let value;
-//     cArr.forEach(val => {
-//         if(val.indexOf(name) === 0)value = val.substring(name.length);
-//     })
-
-//     return value;
-// }
-
-
-
-
-// // Function to set a cookie
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+setCookie = (cName, cValue, expDays) => {
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = cName + "=" + cValue + "; " + expires + "; path =/";
 }
 
-// Function to get a cookie
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
+getCookie = (cName) => {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie); 
+    const cArr = cDecoded.split("; ");
+    let value;
+    cArr.forEach(val => {
+        if(val.indexOf(name) === 0)value = val.substring(name.length);
+    })
 
-// Function to check if a cookie exists
-function cookieExists(name) {
-    return (getCookie(name) !== null);
-}
-
-// Check if the "visits" cookie exists
-if (!cookieExists("visits")) {
-    // If the "visits" cookie does not exist, set it to 1
-    setCookie("visits", 1, 30);
-} else {
-    // If the "visits" cookie exists, increment the value by 1
-    var visits = parseInt(getCookie("visits")) + 1;
-    setCookie("visits", visits, 30);
-}
-
-cookieMessage = () => {
-    if(!getCookie("cookie"))
-        document.querySelector("#cookies").style.display = "block";
+    return value;
 }
 
 document.querySelector("#cookie-btn").addEventListener("click", () => {
     document.querySelector("#cookies").style.display = "none";
-    setCookie("cookie", visits, 30);
-})
+    setCookie("cookie", true, 30);
+    if (!getCookie("visits")) {
+        setCookie("visits", 1, 365);
+    } else {
+        let visits = parseInt(getCookie("visits")) + 1;
+        setCookie("visits", visits, 365);
+    }
+});
+
+cookieMessage = () => {
+    if(!getCookie("cookie"))
+        document.querySelector("#cookies").style.display = "block";
+    else if(getCookie("cookie") === "true") {
+        let visits = parseInt(getCookie("visits")) + 1;
+        setCookie("visits", visits, 365);
+    }
+}
 
 window.addEventListener("load", cookieMessage);
 
+// COOKIES-BUBBLECOUNTER
+
+function addCountToCookie(count) {
+    var d = new Date();
+    d.setTime(d.getTime() + (365*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = "bubbleCount=" + count + ";" + expires + ";path=/";
+}
+
+function getCountFromCookie() {
+    var name = "bubbleCount=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
