50d1f3bf172c31b573fae16b4ee3053f3b25e430
Colin de Koning <colinuithem@gmail.com>
2023-01-25 15:02:21 +0100

    sorteerd nu op basis van datum

--- fetch-events.php
+++ fetch-events.php
@@ -1,7 +1,7 @@
 <?php
 require('../connection.php');
 session_start();
-$sql = $conn->prepare('SELECT * FROM events');
+$sql = $conn->prepare('SELECT * FROM events ORDER BY events.date ASC');
 $sql->execute();
 $fetch = $sql->fetchAll();
 echo json_encode($fetch)
