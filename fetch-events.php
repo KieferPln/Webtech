22e7a5b52c79d229e60a5aee496e36de63d84fe2
Colin de Koning <colinuithem@gmail.com>
2023-01-26 12:55:34 +0100

    changed fetch-events

--- fetch-events.php
+++ fetch-events.php
@@ -1,7 +1,7 @@
 <?php
 require('../connection.php');
 session_start();
-$sql = $conn->prepare('SELECT * FROM events ORDER BY events.date ASC');
+$sql = $conn->prepare('SELECT * FROM events WHERE events.date >= CURDATE() ORDER BY events.date ASC');
 $sql->execute();
 $fetch = $sql->fetchAll();
 echo json_encode($fetch)
