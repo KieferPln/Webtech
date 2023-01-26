ff15d27f41f56c2ba8a68d3c09fd9e02bd00ad9b
Julius de Groot <jjdegroot2001@gmail.com>
2023-01-26 14:32:08 +0100

    ,

--- handleEvents.js
+++ handleEvents.js
@@ -3,12 +3,12 @@ const eventDescription = document.getElementById('event-description')
 const eventUrl = document.getElementById('event-url')
 const eventTags = document.getElementById('event-tags')
 
-const fillEventPopup = (data, tags) => {
+const fillEventPopup = (data,tags) => {
     eventName.appendChild(document.createTextNode(data.name ? data.name : 'Geen naam gevonden'))
     eventDescription.appendChild(document.createTextNode(data.description ? data.description : 'Geen beschrijving gevondeno'))
     eventUrl.setAttribute('href', data.url ? data.url : '')
 
-    for (let i = 0; i < tags.length; i++) { eventTags.appendChild(createTag(tags[i].subject)) }
+    for(let i = 0; i < tags.length; i++){eventTags.appendChild(createTag(tags[i].subject))}
 }
 
 const emptyEventPopup = () => {
@@ -18,7 +18,7 @@ const emptyEventPopup = () => {
     eventUrl.innerHTML = ''
 }
 
-const createTag = (name) => {
+const createTag = (name) =>{
     const tag = document.createElement('div')
     tag.classList.add('tag')
     const nameNode = document.createTextNode(name)
@@ -27,14 +27,12 @@ const createTag = (name) => {
 }
 
 const getTagsByEventId = async (id) => {
-    const response = await fetch("fetch_subjects.php");
-    console.log('ok')
+    const response = await fetch("fetch-tags.php");
     const data = await response.json();
-    if (!data) {
-        return getTagsByEventId();
-    } else {
-        return data.filter((tag) => tag.eventid === id);
-    }
+
+    if (!data) return getTagsByEventId();
+    return data.filter((tag) => tag.eventid == id);
+    
 }
 
 const getEvents = async () => {
@@ -44,5 +42,3 @@ const getEvents = async () => {
     if (!data) return getResult();
     return data;
 }
-
-
